186e7c2d32597715c4a80eebd2d2b972efdde2a8
sbdnd <sebastien.dunand@gmail.com>
2020-10-31 17:20:52 +0100

    architecture de base

---
+++ src/components/movie-details/MovieDetails.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class MovieDetails extends Component {
+    render(){
+        return <div className="w-25 border p-4">Movie Details</div>
+    }
+}
